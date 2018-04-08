99587540a30f816f49637e82f667499f7ac8c51d
Mrinal <waliamrinal@gmail.com>
2018-04-08 23:38:40 +0530

    add these files

---
+++ binary_hex_oct.py
@@ -0,0 +1,12 @@
+# Python program to convert decimal number into binary, octal and hexadecimal number system
+
+# Change this line for a different result
+dec = 12
+
+print("THE NUMBER "+str(dec)+" IN BINARY , HEXADECIMAL AND OCTAL FORM IS-\n")
+
+print(str(bin(dec))+" IN BINARY.\n")
+
+print(str(oct(dec))+" IN OCTAL.\n")
+
+print(str(hex(dec))+" IN HEXADECIMAL.")
