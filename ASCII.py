a5b96e2eb5f3958818ac87af877d3eb93f7cdb46
Mrinal <waliamrinal@gmail.com>
2018-04-10 01:25:42 +0530

    add it

---
+++ ASCII.py
@@ -0,0 +1,5 @@
+#python program to find the ASCII(american standard code for information interchange.
+# ASCII 'A'=65 and ASCII 'a'=32+ASCII 'A'
+char = 'm'
+
+print("THE ASCII VALUE OF "+char+" is "+ str(ord(char)))
