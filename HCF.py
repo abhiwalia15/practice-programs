7a10bd205b62caa0a458149e9610ee6baf540a5a
Mrinal <waliamrinal@gmail.com>
2018-04-10 01:20:16 +0530

    add it

--- HCF.py
+++ HCF.py
@@ -1,1 +1,10 @@
+#python program to find the hcf or greatest common divisor(GCD) of a number
 
+def hcf(a,b):
+	pro = a*b
+	for i in range(1,pro,1):
+		if((a%i==0)and(b%i==0)):
+			hcf = i
+	print("HCF = "+str(hcf))
+
+hcf(21,49)
