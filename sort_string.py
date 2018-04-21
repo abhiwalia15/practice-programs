886ea6a8d69bfda21c6bb67cd66e61d0a9ee8b90
Mrinal <waliamrinal@gmail.com>
2018-04-21 14:02:04 +0530

    add this sorting alphabetically program

--- sort_string.py
+++ sort_string.py
@@ -1,3 +1,17 @@
 #python program to sort the string alphabetically.
 
+my_str = input("ENTER ANY STRING\n")
+#make it suitable for case less comparison.
+my_str = my_str.casefold()
 
+#breakdown the string into a list of strings
+#string() method splits the string at whitespaces and store them into a list.
+words = my_str.split()
+
+#sort the lsit.
+words.sort()
+
+#display the sorted list.
+print("THE SORTED LIST IS:\n")
+for word in words:
+	print(word)
