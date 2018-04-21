0f03fed642e445bfcdd0fa55e0a5a7f7f8eac473
Mrinal <waliamrinal@gmail.com>
2018-04-21 13:38:50 +0530

    add this punctuation removing program

---
+++ punctuations.py
@@ -0,0 +1,14 @@
+#define punctuations
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+my_str = "Hello!!!, he said ---and went."
+
+#make a empty string to add the without punctu. strings
+no_pun = ' '
+
+for char in my_str:
+	if char not in punctuations:
+		no_pun += char
+		
+# display the unpunctuated string
+print(no_pun)
