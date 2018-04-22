253a180a5f1e8cd1f7df4d0d89d6e4ffb9f0dcf9
Mrinal <waliamrinal@gmail.com>
2018-04-22 12:52:57 +0530

    set operation program

---
+++ set_operations.py
@@ -0,0 +1,17 @@
+#python program to find perform different set operations .
+
+#display the two sets.
+E = {0,1,2,3,4,5,6,7,8,9,10}
+N = {2,4,6,8}
+
+#union of sets (U)
+print("UNION =",E | N)
+
+#intersection of sets (n)
+print("INTERSECTION =",E & N)
+
+#difference of sets (-)
+print("DIFFERENCE =",E - N)
+
+#SYMMETRIC DIFFERENCE OF SETS (/_\)
+print("SYMMETRIC DIFFERENCE =",E ^ N)
